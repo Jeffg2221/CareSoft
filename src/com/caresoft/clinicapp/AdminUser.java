110aff1880823cc70e0c510209ef36a1874e11fa
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2023-06-03 11:25:25 -0400

    Initial commit

---
+++ src/com/caresoft/clinicapp/AdminUser.java
@@ -0,0 +1,102 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AdminUser extends User implements HIPAACompliantUser,HIPAACompliantAdmin{
+
+    //... imports class definition...
+
+    // Inside class:
+    private Integer employeeID;
+    private String role;
+    private ArrayList<String> securityIncidents;
+
+    // TO DO: Implement a constructor that takes an ID and a role
+    // TO DO: Implement HIPAACompliantUser!
+    // TO DO: Implement HIPAACompliantAdmin!
+
+    public AdminUser(Integer id , String role){
+        this.employeeID = id;
+        this.role = role;
+    }
+
+
+
+    public void newIncident(String notes) {
+        String report = String.format(
+                "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n",
+                new Date(), this.id, notes
+        );
+        securityIncidents.add(report);
+    }
+    public void authIncident() {
+        String report = String.format(
+                "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n",
+                new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+        );
+        securityIncidents.add(report);
+    }
+
+    // TO DO: Setters & Getters
+
+    @Override
+    public ArrayList<String> reportSecurityIncidents() {
+        return null;
+    }
+
+    @Override
+    public void printSecurityIncidents() {
+        HIPAACompliantAdmin.super.printSecurityIncidents();
+    }
+
+    @Override
+    public boolean adminQATest(ArrayList<String> expectedIncidents) {
+        return HIPAACompliantAdmin.super.adminQATest(expectedIncidents);
+    }
+
+    @Override
+    public boolean assignPin(int pin) {
+        return false;
+    }
+
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID) {
+        return false;
+    }
+
+    public Integer getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(Integer employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public ArrayList<String> getSecurityIncidents() {
+        return securityIncidents;
+    }
+
+    public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+        this.securityIncidents = securityIncidents;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminUser{" +
+                "employeeID=" + employeeID +
+                ", role='" + role + '\'' +
+                ", securityIncidents=" + securityIncidents +
+                ", id=" + id +
+                ", pin=" + pin +
+                '}';
+    }
+}
